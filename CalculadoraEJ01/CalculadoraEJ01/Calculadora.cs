0f72bf6992bd4b275dfb4b76377691f95034756b
bryan.baquedano <bryan.baquedano@UEA-C241>
2025-11-03 09:03:41 +0100

    Agregar archivos de proyecto.

---
+++ CalculadoraEJ01/CalculadoraEJ01/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraEJ01
+{
+    public class Calculadora
+    {
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Restar(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Dividir(int a, int b)
+        {
+            if (b == 0)
+                return double.NaN;
+            return a / b;
+        }
+
+    }
+   
+}
---
+++ CalculadoraEJ01/CalculadoraEJ01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculadoraEJ01
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            Console.WriteLine("==Calculadora Básica==");
+            Console.WriteLine($"Suma: 5 + 3 = {calculadora.Sumar(5,3)} ");
+            Console.WriteLine($"Resta: 5 - 3 = {calculadora.Restar(5, 3)} ");
+            Console.WriteLine($"Multiplicación: 5 * 3 = {calculadora.Multiplicar(5, 3)} ");
+            Console.WriteLine($"División: 6/2 = {calculadora.Dividir(6, 3)} ");
+        }   
+            
+    }
+
+
+
+}

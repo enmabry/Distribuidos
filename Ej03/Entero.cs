05d389f5c1c859928f22a3c28ec2359c12cd0e7e
enmabry <bryanbaquedano11@gmail.com>
2025-11-04 01:18:22 +0200

    Se agrego un nuevo ejercicio

---
+++ Ej03/Entero.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej03
+{
+    public class Entero
+    {
+        private int valor;
+        public Entero(int valorInicial)
+        {
+            this.valor = valorInicial;
+        }
+
+        public int Cuadrado()
+        {
+            return this.valor * this.valor;
+        }
+
+        public int Cubo()
+        {
+            return this.valor * this.valor * this.valor;
+        }
+
+        public int GetValor()
+        {
+            return this.valor;
+        }
+
+        public void SetValor(int nuevoValor)
+        {
+            this.valor = nuevoValor;
+        }
+
+    }
+}
---
+++ Ej03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ej03
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("==EJ03: Clase Entero==");
+
+            Entero entero = new Entero(8);
+
+            Console.WriteLine($"Valor inicial: {entero.GetValor()}");
+            Console.WriteLine($"Cuadrado: {entero.Cuadrado()}");
+            Console.WriteLine($"Cubo: {entero.Cubo()}");
+
+            entero.SetValor(5);
+            Console.WriteLine($"Nuevo valor: {entero.GetValor()}");
+            Console.WriteLine($"Cuadrado: {entero.Cuadrado()}");
+            Console.WriteLine($"Cubo: {entero.Cubo()}");
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+
+
+
+
+
+
+}

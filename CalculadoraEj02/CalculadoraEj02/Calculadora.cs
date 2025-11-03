054e03bd97c5ade0748261bf01949a8147491226
enmabry <bryanbaquedano11@gmail.com>
2025-11-03 09:13:14 +0100

    Se agregaron el Ejercicio02 que usa métodos estáticos.

---
+++ CalculadoraEj02/CalculadoraEj02/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraEJ01
+{
+    public class Calculadora
+    {
+        public static int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Restar(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Dividir(int a, int b)
+        {
+            if (b == 0)
+                return double.NaN;
+            return a / b;
+        }
+
+    }
+   
+}
---
+++ CalculadoraEj02/CalculadoraEj02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculadoraEJ01
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==Calculadora Básica==");
+            Console.WriteLine($"Suma: 5 + 3 = {Calculadora.Sumar(5,3)} ");
+            Console.WriteLine($"Resta: 5 - 3 = {Calculadora.Restar(5, 3)} ");
+            Console.WriteLine($"Multiplicación: 5 * 3 = {Calculadora.Multiplicar(5, 3)} ");
+            Console.WriteLine($"División: 6/2 = {Calculadora.Dividir(6, 3)} ");
+        }   
+            
+    }
+
+
+
+}
